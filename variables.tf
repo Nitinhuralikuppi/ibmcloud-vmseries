362127a8f2df6cc99953e36b7c65e21ca1dbeed0
Nitin Huralikuppi <nitinjh2003@gmail.com>
2023-01-09 14:42:35 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,9 +26,9 @@ variable "subnet_id2"{
   description = "The id of the subnet to which VM-Series VSI's second interface(dataplane) belongs to"
 }
 
-# variable "subnet_id3"{
-#  description = "The id of the subnet to which VM-Series VSI's third interface(trust) belongs to"
-#}
+variable "subnet_id3"{
+  description = "The id of the subnet to which VM-Series VSI's third interface(trust) belongs to"
+}
 
 ##############################################################################
 # ssh_key_name - The name of the public SSH key to be used when provisining VM-Series VSI.
